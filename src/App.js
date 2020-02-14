b8d0c397a4371d3d90b93bc1e56825a76e597732
Matthew French <matthew.french@cmostores.com>
2020-02-14 14:51:55 +0000

    Assignment

--- src/App.js
+++ src/App.js
@@ -1,41 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
-import Person from "./Person/Person";
+import UserInput from "./UserInput/UserInput";
+import UserOutput from "./UserOutput/UserOutput";
+
 
 class App extends Component {
-  state = {
-    persons: [
-      { name: "Matt", age: 28 },
-      { name: "Cass", age: 27 }
-    ]
-  };
+	state = {
+		username: 'Matt'
+	}
 
-  switchNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: "Catherine", age: 27 }
-      ]
-    });
-  };
+	usernameChangedHandler = (event) => {
+		this.setState({
+			username: event.target.value
+		});
+	}
 
   render() {
-    return (
+	return (
       <div className="App">
-        <h1>React App</h1>
-        <p>This is working.</p>
-        <button onClick={() => this.switchNameHandler('Matthew!!')}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-		  age={this.state.persons[0].age}
-		  click={this.switchNameHandler.bind(this, 'Matt!!')}
-        >
-          My hobbies: Gaming
-        </Person>
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        />
+		<UserInput changed={this.usernameChangedHandler} currentName={this.state.username} />
+        <UserOutput username={this.state.username} />
+        <UserOutput username={this.state.username} />
       </div>
     );
   }
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./UserOutput.css";
+
+const userOutput = (props) => {
+  return (
+    <div className="UserOutput">
+        <p>Username: {props.username}</p>
+        <p>
+            Eu non ut ipsum nulla. Tempor reprehenderit sit esse occaecat cupidatat deserunt veniam reprehenderit dolor. Mollit culpa aliquip ad ad labore laboris anim magna eiusmod id velit.
+        </p>
+        <p>
+            Culpa elit esse irure non. Sit pariatur veniam aliquip do mollit amet. Reprehenderit ea enim voluptate ad velit est consequat aliqua laborum qui consequat esse qui. Et cupidatat magna incididunt officia est elit aliquip eu culpa nulla ipsum adipisicing aute officia. Lorem pariatur minim exercitation aliquip in incididunt nulla laboris veniam reprehenderit culpa mollit. Quis laborum voluptate laboris ut aliquip in id. Dolor fugiat occaecat aliquip ad nulla Lorem ullamco.
+        </p>
+    </div>
+  );
+};
+
+export default userOutput;
