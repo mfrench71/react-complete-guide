9248d58be2b5d4475d7ab80cefd1f550f0c5b41b
Matthew French <matthew.french@cmostores.com>
2020-02-14 13:17:17 +0000

    State

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<h1>React App</h1>
-				<p>This is working.</p>
-				<Person />
-				<Person />
-			</div>
-		);
-	}
+  state = {
+    persons: [
+      { name: "Matt", age: 28 },
+      { name: "Cass", age: 27 }
+    ]
+  };
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: "Matthew", age: 28 },
+        { name: "Catherine", age: 27 }
+      ]
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React App</h1>
+        <p>This is working.</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        >
+          My hobbies: Gaming
+        </Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
